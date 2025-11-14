3e1ab5c1085e86fe18573404fef08ff698c486b2
shacesheet <parkseol.dev@gmail.com>
2025-11-14 09:13:31 +0900

    fix: User 도메인 분리

--- src/main/kotlin/com/ps/app/user/application/port/out/UserRepository.kt
+++ src/main/kotlin/com/ps/app/user/application/port/out/UserRepository.kt
@@ -1,13 +1,11 @@
 package com.ps.app.user.application.port.out
 
 import com.ps.app.user.domain.User
-import org.springframework.stereotype.Repository
 
 /**
  * User 리포지토리 포트
  * 도메인 계층에서 정의하는 인터페이스
  */
-@Repository
 interface UserRepository {
     fun save(user: User): User
     fun findById(id: Long): User?
